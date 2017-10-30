37240cf18ef9ffa11b58baa78cd83183fa1ff4a9
mangdehuang <1032036512@qq.com>
2017-10-30 11:59:57 +0800

    install

---
+++ vue-demo-1/src/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="img-list">
+    <div class="img-box">
+      <p class="img-item">
+        <a href="#/button" class="page-link">按钮</a>
+      </p>
+    </div>
+    <div class="img-box">
+      <p class="img-item">
+        <a href="#/list" class="page-link">列表</a>
+      </p>
+    </div>
+    <div class="img-box">
+      <p class="img-item">
+        <a href="#/nav" class="page-link">导航</a>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+  .img-list{
+    width: 300px;
+    height: 200px;
+  }
+  .img-box{
+    background-color: aqua;
+    width: 30%;
+    height: 100%;
+    display: inline-block;
+  }
+  .page-link{
+    margin: 0 auto;
+    text-align: center;
+  }
+  .img-item{
+    margin: 0 auto;
+    position: relative;
+    top:45%;
+  }
+</style>
