5650e65194d5f57d9619f44cd73c835a00eb0218
mangdehuang <1032036512@qq.com>
2017-10-31 16:01:17 +0800

    demo1 nav todo

--- vue-demo-1/src/components/uNav.vue
+++ vue-demo-1/src/components/uNav.vue
@@ -1,6 +1,44 @@
 <template>
+  <div class="uNav" @click="clickNav">
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark" :class="{active:isActive}">{{label}}</div></el-col>
+    </el-row>
+  </div>
 </template>
 <script>
+  export default {
+      name:'uNav',
+      data(){
+          return{
+              isActive:false
+          }
+      },
+      props:{
+          label:{
+              default:'nav'
+          }
+      },
+      methods:{
+        clickNav(){
+            this.isActive = true;
+        }
+      }
+  }
 </script>
 <style scoped>
+  .uNav{
+    width: 100px;
+    height: 30px;
+    display: inline-block;
+  }
+   .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .bg-purple-dark{
+    background: blanchedalmond;
+  }
+  .active{
+    color: orangered;
+  }
 </style>
--- vue-demo-1/src/pages/pageNav.vue
+++ vue-demo-1/src/pages/pageNav.vue
@@ -1,6 +1,37 @@
 <template>
+  <div>
+    <uNav v-for="(item,index) in navlist" :label="item.label" :key="index" :index="index">
+    </uNav>
+  </div>
+
 </template>
 <script>
+  import uNav from '../components/uNav'
+  export default{
+      name:'pageNav',
+      data(){
+          return {
+            navlist:[
+              {
+                label:"首页",
+                value: 0,
+              },
+              {
+                  label:'列表',
+                  value:1,
+              },
+              {
+                  label:'关于',
+                  value:2
+              }
+            ]
+          }
+
+      },
+      components:{
+        uNav
+      }
+  }
 </script>
 <style scoped>
 </style>
